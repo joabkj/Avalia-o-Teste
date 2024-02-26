06ac8a7a92406f91f6bbc6017d7cd5dfae4e27be
joabkj <50465550+joabkj@users.noreply.github.com>
2024-02-25 22:23:20 -0300

    Projeto Avaliação

--- fontes/src/Avaliacao.dpr
+++ fontes/src/Avaliacao.dpr
@@ -2,7 +2,20 @@ program Avaliacao;
 
 uses
   Vcl.Forms,
-  u_admPrincipal in '..\model\u_admPrincipal.pas' {admPrincipal};
+  u_admPrincipal in '..\model\u_admPrincipal.pas' {admPrincipal},
+  u_basAcessoSistema in '..\model\u_basAcessoSistema.pas' {bas_AcessoSistema},
+  Controller.ConfigBanco in '..\controller\Controller.ConfigBanco.pas',
+  Controller.Connection in '..\controller\Controller.Connection.pas',
+  Utils.Criptografia in '..\classes\Utils.Criptografia.pas',
+  Controller.Usuario in '..\controller\Controller.Usuario.pas',
+  u_cadBasico in '..\model\u_cadBasico.pas' {cadBasico},
+  u_dicionarioDados in '..\classes\u_dicionarioDados.pas',
+  u_funcoes in '..\classes\u_funcoes.pas',
+  ucadProdutos in '..\model\ucadProdutos.pas' {cadProdutos},
+  u_cadCategoria in '..\model\u_cadCategoria.pas' {cadCategoria},
+  u_cadEntrada in '..\model\u_cadEntrada.pas' {cad_entrada},
+  u_cadSaida in '..\model\u_cadSaida.pas' {cad_saida},
+  u_cadHistorico in '..\model\u_cadHistorico.pas' {cadHistorico};
 
 {$R *.res}
 
@@ -10,5 +23,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TadmPrincipal, admPrincipal);
+  Application.CreateForm(Tbas_AcessoSistema, bas_AcessoSistema);
+  bas_AcessoSistema.showmodal;
   Application.Run;
 end.
